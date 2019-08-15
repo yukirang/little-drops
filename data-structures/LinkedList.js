98bb59f92d6ed35078096449b5a34d423d6c223a
yukirang <rangk@163.com>
2019-08-15 15:12:26 -0700

    all kinds of remove

--- data-structures/LinkedList.js
+++ data-structures/LinkedList.js
@@ -11,13 +11,21 @@ class LinkedList {
     this.tail = this.head;
     this.length = 0;
   }
-
-  _findNode(index) {
+  _findNodeAt(index) {
     let current = this.head;
     for (let i = 0; i < index; i++) {
       current = current.next;
     }
     return current;
+  }
+  _findNodes(value) {
+    let indexArr = [];
+    let current = this.head;
+    for (let i = 0; i < this.length; i++) {
+      if (current.value === value) indexArr.push(i);
+      current = current.next;
+    }
+    return indexArr;
   }
 
   append(value) {
@@ -52,7 +60,7 @@ class LinkedList {
     // empty
     if (index === 0) return this.prepend(value);
 
-    const pre = this._findNode(index - 1);
+    const pre = this._findNodeAt(index - 1);
     const aft = pre.next;
     const newNode = new Node(value);
 
@@ -62,7 +70,7 @@ class LinkedList {
     return this;
   }
 
-  remove(index) {
+  removeAt(index) {
     let temp = this.head.value;
     // empty or index over range
     if (this.length === 0 || index > this.length - 1 || index < 0) return null;
@@ -75,7 +83,7 @@ class LinkedList {
     if (index === 0) {
       this.head = this.head.next;
     } else {
-      const pre = this._findNode(index - 1);
+      const pre = this._findNodeAt(index - 1);
       temp = pre.next.value;
       // remove tail node
       if (index === this.length - 1) this.tail = pre;
@@ -83,6 +91,35 @@ class LinkedList {
     }
     this.length--;
     return temp;
+  }
+
+  remove(value) {
+    let temp = this.head.value;
+    // empty
+    if (this.length === 0) return null;
+    let index = this.indexOf(value);
+    // remove head node
+    if (index === 0) {
+      this.head = this.head.next;
+    } else {
+      const pre = this._findNodeAt(index - 1);
+      temp = pre.next.value;
+      // remove tail node
+      if (index === this.length - 1) this.tail = pre;
+
+      pre.next = pre.next.next;
+    }
+    this.length--;
+    return temp;
+  }
+
+  removeEvery(value) {
+    const indexArr = this._findNodes(value);
+    if (indexArr && indexArr.length > 0) {
+      for (let i = 0; i < indexArr.length; i++) {
+        this.removeAt(indexArr[i] - i);
+      }
+    }
   }
 
   removeFirst() {
@@ -124,7 +161,7 @@ class LinkedList {
     // empty or index over range
     if (this.length === 0 || index > this.length - 1 || index < 0) return null;
 
-    return this._findNode(index).value;
+    return this._findNodeAt(index).value;
   }
 
   getFirst() {
@@ -170,6 +207,10 @@ class LinkedList {
 //Example
 const myLinkedList = new LinkedList();
 myLinkedList.append(5);
+myLinkedList.append(5);
+myLinkedList.append('hello');
+myLinkedList.append('hello');
+myLinkedList.append('hello');
 myLinkedList.append('hello');
 myLinkedList.prepend(1);
 myLinkedList.insert(2, 3);
@@ -177,7 +218,7 @@ myLinkedList.insert(0, 25);
 myLinkedList.insert(5, 20);
 console.log(myLinkedList.getListArr());
 
-myLinkedList.reverse();
+console.log(myLinkedList.reverse());
 console.log(myLinkedList.getListArr());
 
 console.log(myLinkedList.get(2));
@@ -186,10 +227,18 @@ console.log(myLinkedList.getLast());
 console.log(myLinkedList.contains('hello'));
 console.log(myLinkedList.indexOf('16'));
 
+myLinkedList.removeAt(2);
+console.log(myLinkedList.getListArr());
 myLinkedList.removeFirst();
+console.log(myLinkedList.getListArr());
 myLinkedList.removeLast();
 console.log(myLinkedList.getListArr());
 console.log(myLinkedList.size());
+
+myLinkedList.removeEvery(5);
+console.log(myLinkedList.getListArr());
+myLinkedList.remove('hello');
+console.log(myLinkedList.getListArr());
 
 myLinkedList.clear();
 console.log(myLinkedList.isEmpty());
